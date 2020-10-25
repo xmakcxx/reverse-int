806b0559167f0bd92147576c2e1504b3392f9839
xmakcxx <xmakcxx@mail.ru>
2020-10-25 18:42:14 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let a = Array.from(String(n)).reverse().join('')
+  console.log(a)
+  if(a[a.length-1]==='-'){
+    console.log(a)
+    a = a.replace('-','') ;
+  }
+  if(a[0] === '0'){
+    console.log(a)
+    a = a.replace('0','') ;
+  }
+return Number(a)
 }
